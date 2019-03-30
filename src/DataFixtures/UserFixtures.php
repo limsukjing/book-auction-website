be1fd0e2b1e2b9aaa1d68bc5ad1299054f64bef7
limsukjing <b00100201@student.itb.ie>
2019-03-30 00:55:10 +0000

    create users and role hierarchies

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    // create $passwordEncoder object
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    private function createUser($username, $plainPassword, $roles = ['ROLE_USER']): User
+    {
+        $user = new User();
+        $user->setUsername($username);
+        $user->setRoles($roles);
+
+        // encode password
+        $encodedPassword = $this->passwordEncoder->encodePassword($user, $plainPassword);
+        $user->setPassword($encodedPassword);
+        return $user;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = $this->createUser('admin', 'admin', ['ROLE_SUPER_ADMIN']);
+        $userSeller = $this->createUser('seller', 'seller', ['ROLE_ADMIN']);
+        $userBuyer = $this->createUser('buyer', 'buyer', ['ROLE_ADMIN']);
+
+        $manager->persist($userAdmin);
+        $manager->persist($userSeller);
+        $manager->persist($userBuyer);
+
+        $manager->flush();
+    }
+}

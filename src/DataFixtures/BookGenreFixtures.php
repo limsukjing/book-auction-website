7f94622d665e5e052d41066b04d458628e979ebf
limsukjing <b00100201@student.itb.ie>
2019-04-03 00:14:46 +0100

    update book/genre fixtures and templates

--- src/DataFixtures/BookGenreFixtures.php
+++ src/DataFixtures/BookGenreFixtures.php
@@ -3,13 +3,57 @@
 namespace App\DataFixtures;
 
 use App\Entity\Book;
+use App\Entity\Genre;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class BookFixtures extends Fixture
+class BookGenreFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        /* Genre Fixtures */
+        $g1 = new Genre();
+        $g1->setType('Biography');
+        $g1->setImage('genre_biography.png');
+
+        $g2 = new Genre();
+        $g2->setType('Classic');
+        $g2->setImage('genre_classic.png');
+
+        $g3 = new Genre();
+        $g3->setType('Crime');
+        $g3->setImage('genre_crime.png');
+
+        $g4 = new Genre();
+        $g4->setType('History');
+        $g4->setImage('genre_history.png');
+
+        $g5 = new Genre();
+        $g5->setType('Novel');
+        $g5->setImage('genre_novel.png');
+
+        $g6 = new Genre();
+        $g6->setType('Romance');
+        $g6->setImage('genre_romance.png');
+
+        $g7 = new Genre();
+        $g7->setType('Thriller');
+        $g7->setImage('genre_thriller.png');
+
+        $g8 = new Genre();
+        $g8->setType('Young Adult Fiction');
+        $g8->setImage('genre_yaf.png');
+
+        $manager->persist($g1);
+        $manager->persist($g2);
+        $manager->persist($g3);
+        $manager->persist($g4);
+        $manager->persist($g5);
+        $manager->persist($g6);
+        $manager->persist($g7);
+        $manager->persist($g8);
+
+        /* Book Fixtures */
         $b1 = new Book();
         $b1->setTitle('Becoming');
         $b1->setAuthor('Michelle Obama');
@@ -17,7 +61,7 @@ class BookFixtures extends Fixture
         $b1->setISBN(9783442314874);
         $b1->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b1->setReservePrice('29.99');
-        $b1->setGenre(null);
+        $b1->setGenre($g1);
 
         $b2 = new Book();
         $b2->setTitle('To All the Boys I\'ve Loved Before');
@@ -26,7 +70,7 @@ class BookFixtures extends Fixture
         $b2->setISBN(9782809463675);
         $b2->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b2->setReservePrice('6.50');
-        $b2->setGenre(null);
+        $b2->setGenre($g8);
 
         $b3 = new Book();
         $b3->setTitle('Gone Girl');
@@ -35,7 +79,7 @@ class BookFixtures extends Fixture
         $b3->setISBN(9782809463675);
         $b3->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b3->setReservePrice('10.20');
-        $b3->setGenre(null);
+        $b3->setGenre($g7);
 
         $b4 = new Book();
         $b4->setTitle('Crazy Rich Asians');
@@ -44,7 +88,7 @@ class BookFixtures extends Fixture
         $b4->setISBN(	9780385536974);
         $b4->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b4->setReservePrice('18.99');
-        $b4->setGenre(null);
+        $b4->setGenre($g5);
 
         $b5 = new Book();
         $b5->setTitle('To Kill a Mockingbird');
@@ -53,7 +97,7 @@ class BookFixtures extends Fixture
         $b5->setISBN(	9780446310789);
         $b5->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b5->setReservePrice('39.99');
-        $b5->setGenre(null);
+        $b5->setGenre($g2);
 
         $b6 = new Book();
         $b6->setTitle('Pride and Prejudice');
@@ -62,7 +106,7 @@ class BookFixtures extends Fixture
         $b6->setISBN(	9780446310789);
         $b6->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b6->setReservePrice('50.50');
-        $b6->setGenre(null);
+        $b6->setGenre($g2);
 
         $b7 = new Book();
         $b7->setTitle('The Notebook');
@@ -71,7 +115,7 @@ class BookFixtures extends Fixture
         $b7->setISBN(	9785170797455);
         $b7->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b7->setReservePrice('5.00');
-        $b7->setGenre(null);
+        $b7->setGenre($g6);
 
         $b8 = new Book();
         $b8->setTitle('The Fault in Our Stars');
@@ -80,7 +124,7 @@ class BookFixtures extends Fixture
         $b8->setISBN(	9780141345659);
         $b8->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b8->setReservePrice('15.00');
-        $b8->setGenre(null);
+        $b8->setGenre($g8);
 
         $b9 = new Book();
         $b9->setTitle('Murder on the Orient Express');
@@ -89,7 +133,7 @@ class BookFixtures extends Fixture
         $b9->setISBN(	9788497571838);
         $b9->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b9->setReservePrice('66.40');
-        $b9->setGenre(null);
+        $b9->setGenre($g3);
 
         $b10 = new Book();
         $b10->setTitle('The Diary of A Young Girl');
@@ -98,7 +142,7 @@ class BookFixtures extends Fixture
         $b10->setISBN(	9780307832375);
         $b10->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b10->setReservePrice('193.00');
-        $b10->setGenre(null);
+        $b10->setGenre($g1);
 
         $b11 = new Book();
         $b11->setTitle('My Sister\'s Keeper');
@@ -107,7 +151,7 @@ class BookFixtures extends Fixture
         $b11->setISBN(	9789616588126);
         $b11->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b11->setReservePrice('17.60');
-        $b11->setGenre(null);
+        $b11->setGenre($g5);
 
         $b12 = new Book();
         $b12->setTitle('Extremely Loud & Incredibly Close');
@@ -116,7 +160,7 @@ class BookFixtures extends Fixture
         $b12->setISBN(	9784140056035);
         $b12->setSummary('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nunc libero, porttitor eget accumsan at, convallis id orci. Maecenas tristique nisi in rhoncus gravida. Aliquam erat volutpat. Integer rhoncus massa quis aliquam tristique. Nunc eget orci consequat, gravida orci malesuada, consectetur dui. Donec metus nisi, euismod non arcu at, molestie pretium metus. Nam mattis sed nunc id ornare. Pellentesque pharetra ac massa in bibendum. Duis in erat cursus, vehicula orci ac, ullamcorper lacus. Donec pharetra mauris et nisl cursus mattis. Nam vulputate ultricies elit vel condimentum. Vestibulum orci urna, pellentesque vitae mauris sed, sagittis tristique urna. Phasellus gravida vulputate diam nec aliquet. Nunc ac consectetur odio.');
         $b12->setReservePrice('22.30');
-        $b12->setGenre(null);
+        $b12->setGenre($g5);
 
         $manager->persist($b1);
         $manager->persist($b2);
